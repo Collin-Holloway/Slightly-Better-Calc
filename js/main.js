f2e4c1ce3b61eeb043a8e8a16f4c0a937ab36c9f
Collin Holloway <collinjholloway@gmail.com>
2022-09-02 16:08:09 -0500

    Initial java commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
-let total = 0
-let num1 = 0
-let num2 = 0
-let operator = ''
+//Required abilities of a calculator:
+//accept user inputs of number, operator, and number
+//should accept decimal numbers
+//store inputs
+//recognize inputs and perform calculations
+//reutrn a result
 
-let display = ''
+//Optional features:
+//Should accept longer arithmetic operations
+//display all input as it is being entered
+//store previous total as start of next operation
+//clear button should clear all entries
+//should prevent invalid inputs (operators next to each other)
+
+const keys = document.querySelector('.calculator-buttons');
+    keys.addEventListener('click', event => {
+        const {target} = event;
+        const {value} = target;
+        if(!target.matches('button')){
+            return;
+        }else{
+            calculator.parseInput(value)
+            //console.log(value)
+        }
+    })
+
+    const calculator = {
+        displayText: '0',
+        prevTotal: null,
+        parseInput(value){
+            
+        }
+    }
